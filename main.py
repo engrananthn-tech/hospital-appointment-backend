1111b508c9e6e286feb7d21480b09df320b8dc26
Anant <engr.ananthn@gmail.com>
2026-03-03 23:41:35 +0530

    refactor:add git hub link to landing page

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 def home():
@@ -22,12 +21,14 @@ Open /docs to explore and test the API.<p>
 
 <p>If you're reviewing the project:
 See the GitHub repository for architecture and implementation details.</p>
+<p><a href="https://github.com/engrananthn-tech/hospital-appointment-backend">Git hub repo<a><p>
+
         </body>
     </html>
     """
 
 app.include_router(user.router)
-app.include_router(auth.router)
+app.include_router(auth.router) 
 app.include_router(doctor.router)
 app.include_router(slot.router)
 app.include_router(appointment.router)
